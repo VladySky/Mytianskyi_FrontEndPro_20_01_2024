5fe2bf82fc2660dad42797fce078ce8b405dd357
Vlady <vlad.mytia@gmail.com>
2024-01-25 21:17:38 +0100

    half of the work

---
+++ main.js
@@ -0,0 +1,11 @@
+let x = +prompt(`How old are you?`);
+console.log(x) ;
+
+let y = +prompt(`Day of your birthday?`);
+console.log(y);
+
+let z = +prompt(`Your month of birthday?`);
+console.log(z);
+
+
+
