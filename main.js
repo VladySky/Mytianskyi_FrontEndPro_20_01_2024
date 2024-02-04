83a252135fdfa81d68e02c709c12adfaf174da62
Vlady <vlad.mytia@gmail.com>
2024-02-04 22:16:24 +0100

    hw4

---
+++ main.js
@@ -0,0 +1,15 @@
+let number1 = +prompt(`Write the first number`);
+
+let number2 = +prompt(`Write second number`)
+
+let summery1 = number1 + number2
+let summery2 = number1 - number2
+let summery3 = number1 * number2
+let summery4 = number1 / number2
+
+
+alert(`User wrote ${number1} and ${number2}
+    ${number1} + ${number2} = ${summery1}
+    ${number1} - ${number2} = ${summery2}
+    ${number1} * ${number2} = ${summery3}
+    ${number1} / ${number2} = ${summery4} `)
